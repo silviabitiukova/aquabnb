42bbec0084c047d97c15a688ea9e845cdc596e20
Simone Totino <totino.simone@gmail.com>
2021-02-24 17:45:25 +0200

    new schema migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_180927) do
+ActiveRecord::Schema.define(version: 2021_02_24_131738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_180927) do
     t.integer "passenger_capacity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "boat_image"
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
