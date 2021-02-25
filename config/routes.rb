878fb41d4b1eb173c63d086d12d523885baa3f07
Simone Totino <totino.simone@gmail.com>
2021-02-25 13:22:05 +0200

    addes bookings routes for actions #new and #create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :boats 
+  resources :boats do
+    resources :bookings, only: [:new, :create]
+  end
 end
